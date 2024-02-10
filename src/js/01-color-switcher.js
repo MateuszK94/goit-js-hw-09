9b00315aeca951e57f4b0dfe49c43276278d636b
MateuszK94 <mateuszk0894@gmail.com>
2024-02-10 12:50:05 +0100

    change html and js code

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
-console.log("test");
+function getRandomHexColor() {  
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let intervalId = null;
+
+document.getElementById('startButton').addEventListener('click', function() {
+    if (!intervalId) {
+        intervalId = setInterval(function() {
+            document.body.style.backgroundColor = getRandomHexColor();
+        }, 1000);
+        document.getElementById('startButton').setAttribute('disabled', true);
+        document.getElementById('stopButton').removeAttribute('disabled');
+    }
+});
+
+document.getElementById('stopButton').addEventListener('click', function() {
+    clearInterval(intervalId);
+    intervalId = null;
+    document.getElementById('startButton').removeAttribute('disabled');
+    document.getElementById('stopButton').setAttribute('disabled', true);
+});
