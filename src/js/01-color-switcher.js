ad2f34213e2e53b0660465be4772ca5e64394404
MateuszK94 <mateuszk0894@gmail.com>
2024-02-10 12:59:00 +0100

    prettier format

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,22 +1,22 @@
-function getRandomHexColor() {  
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
 let intervalId = null;
 
-document.getElementById('startButton').addEventListener('click', function() {
-    if (!intervalId) {
-        intervalId = setInterval(function() {
-            document.body.style.backgroundColor = getRandomHexColor();
-        }, 1000);
-        document.getElementById('startButton').setAttribute('disabled', true);
-        document.getElementById('stopButton').removeAttribute('disabled');
-    }
+document.getElementById('startButton').addEventListener('click', function () {
+  if (!intervalId) {
+    intervalId = setInterval(function () {
+      document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    document.getElementById('startButton').setAttribute('disabled', true);
+    document.getElementById('stopButton').removeAttribute('disabled');
+  }
 });
 
-document.getElementById('stopButton').addEventListener('click', function() {
-    clearInterval(intervalId);
-    intervalId = null;
-    document.getElementById('startButton').removeAttribute('disabled');
-    document.getElementById('stopButton').setAttribute('disabled', true);
+document.getElementById('stopButton').addEventListener('click', function () {
+  clearInterval(intervalId);
+  intervalId = null;
+  document.getElementById('startButton').removeAttribute('disabled');
+  document.getElementById('stopButton').setAttribute('disabled', true);
 });
